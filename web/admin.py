ca7877b6518ea42d5e05e8254c7a0111963ce527
Phoenix <aliheydari335@gmail.com>
2020-02-13 16:39:04 +0330

    sec commit

--- web/admin.py
+++ web/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import  Expese, Income
+
 # Register your models here.
+
+admin.site.register(Expese)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+class Expese(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+
+class Income(models.Model):
+    text = models.CharField(max_length= 255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
